7fbe48fbdeb54f6efa77d5dd4afeb13c7c899fe1
Dawid Tomasiewicz <tomasiewicz.dawid@gmail.com>
2019-03-20 23:04:48 +0100

    srand

--- reduce.c
+++ reduce.c
@@ -4,8 +4,7 @@
 #include <time.h>
 
 double get_pi(sqr){
-    srand(clock());
-    srand(time(NULL));
+
     double i;
     double count = 0;
     for (i = 0; i < sqr; i++){
@@ -30,62 +29,86 @@ int main(int argc, char** argv) {
   double number;
   int iter_num = atoi(argv[1]);
   if (world_rank == 0) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
       double pi = *numbers/((double) world_size);
       printf("\nPi is %f\n", pi);
 
   } else if (world_rank == 1) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 2) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 3) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 4) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 5) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 6) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 7) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 8) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 9) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 10) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   } else if (world_rank == 11) {
+      srand(clock());
+      srand(time(NULL) + world_rank);
       number = get_pi(iter_num);
       printf("\nPi in %d is %f\n", world_rank, number);
       MPI_Reduce(&number, numbers, 1,  MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
